33a79fec1f25f95a667989736bd20a96ed115883
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-20 10:21:40 +0800

    Create 102.java

---
+++ Solutions/102.java
@@ -0,0 +1,36 @@
+// 二叉树的层次遍历
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        } 
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                level.add(node.val);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
