662dce5a883f35a53bca073557c3a95e9ac77fb5
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-20 20:42:47 +0800

    Create 429.java

---
+++ Solutions/429.java
@@ -0,0 +1,45 @@
+// N叉树的层次遍历
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; ++i) {
+                Node node = queue.poll();
+                level.add(node.val);
+                for (Node child : node.children) {
+                    if (child != null) {
+                        queue.add(child);
+                    }
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
