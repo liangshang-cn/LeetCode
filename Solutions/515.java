f31d413b727219c3facd63f6052e71cb383c4b54
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-20 20:43:40 +0800

    Create 515.java

---
+++ Solutions/515.java
@@ -0,0 +1,27 @@
+// 在二叉树每行中寻找最大值
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(max);
+        }
+        return res; 
+    }
+}
