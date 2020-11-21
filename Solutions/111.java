ba9a71ef190b80a311b99444f21f2b0fa62abfc0
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-21 19:20:35 +0800

    Create 111.java

---
+++ Solutions/111.java
@@ -0,0 +1,18 @@
+// 二叉树的最小深度
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null) {
+            return 1 + minDepth(root.right);
+        } else if (root.right == null) {
+            return 1 + minDepth(root.left);
+        } else {
+            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+    }
+}
